901b52daa3a0aa0d695da5e555990bed9330b59f
Minh Tam <minhtamck4@gmail.com>
2016-08-19 10:27:36 +0700

    Add file

---
+++ js/l10n.js
@@ -0,0 +1,45 @@
+/**
+ *  @name L10n
+ *  @description Localization
+ *  @version 1.0
+ */
+var L10n = {
+  searchForm: {
+    'search_top': {
+      required: 'Obligatoire / ne peut être vide'
+    }
+  },
+  observationForm: {
+    'enter-info': {
+      required: 'Obligatoire / ne peut être vide'
+    },
+    gender: {
+      required: 'Obligatoire / ne peut être vide'
+    },
+    'last-name': {
+      required: 'Obligatoire / ne peut être vide'
+    },
+    'first-name': {
+      required: 'Obligatoire / ne peut être vide'
+    },
+    'career': {
+      required: 'Obligatoire / ne peut être vide'
+    },
+    'your-email': {
+      required: 'Obligatoire / ne peut être vide',
+      email: 'Format non valide'
+    },
+    'nature-advice': {
+      required: 'Obligatoire / ne peut être vide'
+    },
+    'observation-message': {
+      required: 'Obligatoire / ne peut être vide',
+      maxlength: ''
+    }
+  },
+  helpForm:{
+    'input-search-help':{
+      required: 'Obligatoire / ne peut être vide'
+    }
+  }
+};
