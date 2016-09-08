e9356e970977f328233c854496174a43183f0ecb
Minh Tam <minhtamck4@gmail.com>
2016-09-08 19:46:18 +0700

    deloy code

--- js/script.js
+++ js/script.js
@@ -5,41 +5,77 @@
  */
 jQuery(function() {
     // Slide footer
-    $('.single-slick').slick({
-        dots: true,
-        arrows: false
-    });
+    // $('.single-slick').slick({
+    //     dots: true,
+    //     arrows: false
+    // });
 
-    // Init trigger scroll
-    var $animation_elements = $('.animation-element,.bounce-element');
-    var $window = $(window);
-    var check_if_in_view = function() {
-        var window_height = $window.height();
-        var window_top_position = $window.scrollTop();
-        var window_bottom_position = (window_top_position + window_height);
-        $.each($animation_elements, function() {
-            var $element = $(this);
-            var element_height = $element.outerHeight();
-            var element_top_position = $element.offset().top;
-            var element_bottom_position = (element_top_position + element_height);
+    // // Init trigger scroll
+    // var $animation_elements = $('.animation-element,.bounce-element');
+    // var $window = $(window);
+    // var check_if_in_view = function() {
+    //     var window_height = $window.height();
+    //     var window_top_position = $window.scrollTop();
+    //     var window_bottom_position = (window_top_position + window_height);
+    //     $.each($animation_elements, function() {
+    //         var $element = $(this);
+    //         var element_height = $element.outerHeight();
+    //         var element_top_position = $element.offset().top;
+    //         var element_bottom_position = (element_top_position + element_height);
 
-            //check to see if this current container is within viewport
-            if ((element_bottom_position >= window_top_position) &&
-                (element_top_position <= window_bottom_position)) {
-                $element.addClass('in-view');
-            } else {
-                $element.removeClass('in-view');
-            }
+    //         //check to see if this current container is within viewport
+    //         if ((element_bottom_position >= window_top_position) &&
+    //             (element_top_position <= window_bottom_position)) {
+    //             $element.addClass('in-view');
+    //         } else {
+    //             $element.removeClass('in-view');
+    //         }
+    //     });
+    // }
+    // $window.on('scroll resize', check_if_in_view);
+    // $window.trigger('scroll');
+    
+    /* Reset svg file */
+    (function resetSvg () {
+        jQuery('img.svg').each(function(){
+            var $img = jQuery(this);
+            var imgID = $img.attr('id');
+            var imgClass = $img.attr('class');
+            var imgURL = $img.attr('src');
+        
+            jQuery.get(imgURL, function(data) {
+                // Get the SVG tag, ignore the rest
+                var $svg = jQuery(data).find('svg');
+        
+                // Add replaced image's ID to the new SVG
+                if(typeof imgID !== 'undefined') {
+                    $svg = $svg.attr('id', imgID);
+                }
+                // Add replaced image's classes to the new SVG
+                if(typeof imgClass !== 'undefined') {
+                    $svg = $svg.attr('class', imgClass+' replaced-svg');
+                }
+        
+                // Remove any invalid XML tags as per http://validator.w3.org
+                $svg = $svg.removeAttr('xmlns:a');
+                
+                // Check if the viewport is set, else we gonna set it if we can.
+                if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+                    $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+                }
+        
+                // Replace image with new SVG
+                $img.replaceWith($svg);
+        
+            }, 'xml');
         });
-    }
-    $window.on('scroll resize', check_if_in_view);
-    $window.trigger('scroll');
+    })();
 
     // Init google map
     function initialize() {
         var mapProp = {
-            center: new google.maps.LatLng(51.508742, -0.120850),
-            zoom: 5,
+            center: new google.maps.LatLng(10.8166418, 106.6738968),
+            zoom: 12,
             mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         var map = new google.maps.Map(document.getElementById("map_canvas"), mapProp);
